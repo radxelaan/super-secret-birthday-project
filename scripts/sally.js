c3eb3465ad85487afc8a5f35892525478273b1f0
Marius2927 <mprevent@csd.auth.gr>
2023-07-26 15:58:25 +0300

    mike joins the battle

--- scripts/sally.js
+++ scripts/sally.js
@@ -1,8 +1,15 @@
-let down = true;
+let gob;
+let gobPos;
+let down = false;
 let count = 0;
 let wait = false;
 let waitTime;
 let prevWait=0;
+let keyCodes = ['87', '65', '83', '68', '81', '69', '82', '84', '89', '73', '74', '75', '76', '90', '88'];
+let keyLabels = ['W', 'A', 'S', 'D', 'Q', 'E', 'R', 'T', 'Y', 'I', 'J', 'K', 'L', 'Z', 'X'];
+let key;
+let command;
+let strikes = 0;
 
 function start(){
     document.getElementsByTagName("audio")[0].volume = '0.2';
@@ -20,8 +27,11 @@ function start(){
                         go.style.marginTop = "5%";
                         go.src = "images\\go.png";
                         bringSallyUp();
+                        getReadyMike();
                         setTimeout(function(){
                             go.style.visibility = "hidden";
+                            key = document.getElementById("key");
+                            key.style.visibility = "visible";
                         }, 1000);
                     }, 1000);
                 }, 1000);
@@ -31,15 +41,20 @@ function start(){
 }
 
 async function bringSallyUp() {
-    let gob = document.getElementById("goblin");
-    let gobPos = gob.src.split("images/")[1];
-    if (gobPos == "sarabacharMid.png" || gobPos == "sarabacharDefeat1.png" || gobPos == "sarabacharDefeat2.png"){
+    gob = document.getElementById("goblin");
+    gobPos = gob.src.split("images/")[1];
+    if (gobPos == "sarabacharMid.png" && gobPos != "sarabacharDefeat1.png" && gobPos != "sarabacharDefeat2.png"){
         waitTime = 200;
-        if(count == 29 && !down){
+        if(count == 29 && down){
             waitTime = 1000;
             gobPos = "sarabacharDefeat1.png";
-            count++;
+            if (document.getElementById("text").src != "images\\defeat.png"){
+                victory();
+            }
         }
+    }
+    else if(gobPos == "sarabacharDefeat1.png" || gobPos == "sarabacharDefeat2.png"){
+        waitTime = 200;
     }
     else{
         waitTime = 1400;
@@ -60,14 +75,27 @@ async function bringSallyUp() {
     setTimeout(function(){
         if(!wait && gobPos == "sarabacharUp.png"){
             gob.src = "images\\sarabacharMid.png";
-            down = true;
         }
-        else if (!wait && gobPos == "sarabacharMid.png" && down){
+        else if (!wait && gobPos == "sarabacharMid.png" && !down){
             gob.src = "images\\sarabacharDown.png";
+            setTimeout(function(){
+                command = Math.floor(Math.random()*keyCodes.length);
+                key.innerHTML = keyLabels[command];
+            }, 700);
+            setTimeout(function(){
+                down = true;
+            }, 175);
         }
         else if (!wait && gobPos == "sarabacharMid.png"){
             gob.src = "images\\sarabacharUp.png";
             count++;
+            setTimeout(function(){
+                command = Math.floor(Math.random()*keyCodes.length);
+                key.innerHTML = keyLabels[command];
+            }, 700);
+            setTimeout(function(){
+                down = false;
+            }, 175);
         }
         else if (gobPos == "sarabacharDefeat1.png") {
             gob.src = "images\\sarabacharDefeat2.png";
@@ -77,9 +105,92 @@ async function bringSallyUp() {
         }
         else if (!wait && gobPos != "sarabacharDefeat2.png" && gobPos != "sarabacharDefeat2.png") {
             gob.src = "images\\sarabacharMid.png";
-            down = false;
         }
         prevWait = waitTime;
+        key.style.backgroundColor = "aquamarine";
         bringSallyUp()
     }, waitTime);
+}
+
+function getReadyMike(){
+   let mike = document.getElementById("mike");
+   setTimeout(function(){
+        mike.src = "images\\mikeMid.png";
+        setTimeout(function(){
+            mike.src = "images\\mikeDown.png";
+            document.addEventListener("keydown", keyDownHandler);
+        }, 200);
+    }, 1400);
+}
+
+function keyDownHandler(e){
+    let mike = document.getElementById("mike");
+    let text = document.getElementById("text");
+    console.log(down)
+    if(e.keyCode == keyCodes[command] && mike.src.split("images/")[1] == "mikeUp.png" && !down){
+        mike.src = "images\\mikeMid.png";
+        key.style.backgroundColor = "lime";
+        text.src = "images\\good.png";
+        text.style.visibility = "visible";
+        setTimeout(function(){
+            text.style.visibility = "hidden";
+        }, 400);
+        setTimeout(function(){
+            mike.src = "images\\mikeDown.png";
+        }, 200);
+    }
+    else if(!wait && e.keyCode == keyCodes[command] && mike.src.split("images/")[1] == "mikeDown.png" && down){
+        mike.src = "images\\mikeMid.png";
+        key.style.backgroundColor = "lime";
+        text.src = "images\\good.png";
+        text.style.visibility = "visible";
+        setTimeout(function(){
+            text.style.visibility = "hidden";
+        }, 400);
+        setTimeout(function(){
+            mike.src = "images\\mikeUp.png";
+        }, 200);
+    }
+    else{
+        key.style.backgroundColor = "red";
+        text.src = "images\\tooLate.png";
+        text.style.visibility = "visible";
+        setTimeout(function(){
+            text.style.visibility = "hidden";
+        }, 200);
+        strikes++;
+        if(strikes>=3){
+            defeat();
+        }
+    }
+
+}
+
+function victory(){
+    setTimeout(function(){
+        document.removeEventListener("keydown", keyDownHandler);
+        key.style.visibility = "hidden";
+    }, 1000);
+    setTimeout(function(){
+        document.getElementsByTagName("audio")[0].src = "audio\\Boss Win - WarioWare, Inc. Mega Microgames! (OST).ogg";
+        document.getElementsByTagName("audio")[0].loop=false
+        text.style.visibility = "visible";
+        text.src = "images\\victory.png";
+        text.style.width= "450px";
+        text.style.marginLeft= "42%";
+    }, 13000);
+}
+
+function defeat(){
+    document.removeEventListener("keydown", keyDownHandler);
+    document.getElementsByTagName("audio")[0].pause();
+    key.style.visibility = "hidden";
+    setTimeout(function(){
+        document.getElementsByTagName("audio")[0].src = "audio\\Boss Loss - WarioWare, Inc. Mega Microgames! (OST).ogg";
+        document.getElementsByTagName("audio")[0].loop=false
+        text.style.visibility = "visible";
+        text.style.width= "500px";
+        text.style.marginLeft= "40%";
+        text.src = "images\\defeat.png";
+    }, 300);
 }
