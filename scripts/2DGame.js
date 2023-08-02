413e2ec4c94e6d4fa9a7e35fc3b4b9af1e4d0c31
radxelaan <alexandraap20@gmail.com>
2023-08-02 19:39:02 +0300

    Fixed normalization wooo

--- scripts/2DGame.js
+++ scripts/2DGame.js
@@ -13,10 +13,50 @@ render.options.wireframes = false
 eq = ''
 x = Array.from(Array(w).keys())
 x = x.map(point => { return point -= w / 2 })
+y = Array.from(Array(h).keys())
+y = y.map(point => { return point -= h / 2 })
 bodies = []
+let scale_factor = 70
+let points = []
+let lines = []
+const equations = []
 
-function equation(x) {
-  return h - (eval(eq) * h/100 + h / 2)
+function equation(x, eq) {
+  return eval(eq)
+}
+
+function scaleLinesX(){
+  let scaleLines = []
+  x.forEach(point => {
+    if (!(Math.round(point) % scale_factor)) {
+      scaleLines.push(Matter.Bodies.rectangle(point + w/2, h / 2, 1, 10, { isStatic: true }))
+    }
+  })
+  return scaleLines
+}
+
+function scaleLinesY(){
+  let scaleLines = []
+  x.forEach(point => {
+    if (!(Math.round(point) % scale_factor)) {
+      scaleLines.push(Matter.Bodies.rectangle(w / 2, point + h / 2, 10, 1, { isStatic: true }))
+    }
+  })
+  return scaleLines
+}
+
+function plot(eq) {
+  const p = []
+  x.forEach(point => {
+    p.push(Matter.Bodies.rectangle(point + w / 2, h - equation(point/scale_factor, eq)*scale_factor - h/2, 1, 1,{ 
+      isStatic: true,
+      render: {
+        strokeStyle: 'blue'
+      } 
+    }))
+  })
+  lines.push(p)
+  return p
 }
 
 let ball = Matter.Bodies.circle(700,100,20)
@@ -35,6 +75,33 @@ yAxis.collisionFilter = {
 'category': 2,
 'mask': 0,
 }
+
+let linesX = scaleLinesX()
+Matter.World.add(engine.world, linesX)
+
+let linesY = scaleLinesY()
+Matter.World.add(engine.world, linesY)
+
+document.querySelector('#zoom').addEventListener("click", function () {
+  Matter.World.remove(engine.world, linesX)
+  Matter.World.remove(engine.world, linesY)
+  lines.forEach(line => {
+    Matter.World.remove(engine.world, line)
+  })
+  console.log(lines)
+  scale_factor+=1
+  linesX = scaleLinesX()
+  linesY = scaleLinesY()
+  lines = []
+  equations.forEach(equation => {
+    lines.push(plot(equation))
+  })
+  Matter.World.add(engine.world, linesX)
+  Matter.World.add(engine.world, linesY)
+  lines.forEach(line => {
+    Matter.World.add(engine.world, line)
+  })
+})
  
 bodies.push(ball)
 bodies.push(xAxis)
@@ -47,15 +114,7 @@ Matter.Render.run(render)
 
 document.querySelector('#submit').addEventListener("click", function () {
   eq = `(${document.querySelector('#equation').value})`
-  const points = []
-  x.forEach(point => {
-    points.push(Matter.Bodies.rectangle(point + w / 2, equation((point + 5) / 10 ), 1, 1,{ 
-      isStatic: true,
-      render: {
-        strokeStyle: 'blue'
-      } 
-    }))
-  })
-  console.log(points)
-  Matter.World.add(engine.world, points)
+  equations.push(eq)
+  lines.push(plot(eq))
+  Matter.World.add(engine.world, lines[lines.length - 1])
 })
